726194319912d102b1f58bce5f8ae7340a098d09
jsdeemon <jsdeemon@gmail.com>
2021-12-13 07:51:59 +0500

    made some changes for deploy

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,7 @@ if(process.env.NODE_ENV === 'development') {
 // adding body parser 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+
 
 // use Product routes 
 app.use("/api/products", productRoutes)
@@ -53,9 +51,22 @@ app.use('/api/upload', uploadRoutes)
 // PayPal config 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
+
 // making upload folder static 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    })
+}
+
+
 
 // not found
 app.use(notFound)
